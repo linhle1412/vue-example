f98bdf86d5a61e4e44618174238656d03968c0aa
Linh Le <hiolinh@gmail.com>
2021-09-17 23:27:08 +0700

    fix sth

--- pages/index.vue
+++ pages/index.vue
@@ -89,6 +89,57 @@ export default {
       interval: null
     };
   },
+  head() {
+    return {
+      title: 'Quỹ Phát triển tài năng Việt',
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Quỹ Phát triển tài năng Việt"
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://quyphattrientainangviet.akadigital.vn"
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: "Quỹ Phát triển tài năng Việt"
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: "Quỹ Phát triển tài năng Việt"
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "article"
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: '/icon.jpg'
+        },
+        {
+          hid: 'image',
+          name: 'image',
+          content: '/icon.jpg'
+        },
+        {
+          hid: 'fb:app_id',
+          property: 'fb:app_id',
+          content: '810401282889398'
+        },
+      ],
+    }
+   
+  },
   created() {
     this.$store.dispatch('fetchFund')
   },
